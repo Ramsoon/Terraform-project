dedc230dd7b01a86ec7827287bb5e3dec3883618
SADIQ ABDULRAHAMAN <40242872+Ramsoon@users.noreply.github.com>
2025-08-07 13:57:44 +0100

    Update main.tf

--- instance/main.tf
+++ instance/main.tf
@@ -39,6 +39,8 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_key_pair" "mykey" {
   key_name   = "myec2-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDpbrKPv/pqEVSEoMIs85qKwUZneq5rdWcxxkQsczvnZengKbBgFMZmgjh3MiTO6rbwctt4Iccl+CFJagHxZ2758O2cAwxGAp2UVAffuLcH38+q/DbWYR4ySGhvYEaUs7TW0ppiFb0sUr79fS5mdbr+EbeBFO/WFbLNVzDpvhnuqe3+zrhWZNzhlj37IOYcJSoLZevGXbqXcXOyozHxDpFrsMr18rYeZPODbEH0N2qqwgAdQwYrH9pqFz1Pz9bHI30W+DWf3L8p4AB1bHU/hlJIarNB8c93fM8M60EBiSyaYnmYc2nWbksvoHZq/ncBLWs1ZwgDBnoqqtf7KpCeGOwJUiieRJO2QT3a+z4gDL1gC5ixPVu3Z2F5DP6kMiAQezYEwYHN2H18cd+BOTTf3nYXgvilc5m4iDO6m1iIWZxOeJnXNA7xqa0/OPiY7it47jDlz9illIMSt2CriPaytoQ9Xgk8xG22PYswEPi47Bz2eLSWF+eCTsbut7aHlOIXjyE="
+  public_key = file("${path.module}/mykey.pub")
+
 }
+
  
